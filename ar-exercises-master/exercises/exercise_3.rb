e1d6157f62f4e09cc7ed0330c2f30c4beac658b5
Rayed <siddiqui.rayed@gmail.com>
2016-05-11 19:21:45 -0400

    deleted extra ar-exerercise directory

--- ar-exercises-master/exercises/exercise_3.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-
-puts "Exercise 3"
-puts "----------"
-
-# Your code goes here ...
--- ar-exercises-master/exercises/exercise_4.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-
-puts "Exercise 4"
-puts "----------"
-
-# Your code goes here ...
